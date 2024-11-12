c3178811da7d80ff9e26ef97a6e64749b7bbe867
cynthia <61641064+cor100@users.noreply.github.com>
2024-11-12 14:43:20 -0500

    deal with contractions

--- clean_data.py
+++ clean_data.py
@@ -1,6 +1,8 @@
 import pandas as pd
 import spacy
 import string
+import contractions
+import json
 
 nlp = spacy.load("en_core_web_sm")
 file = 'MI Dataset.csv'
@@ -8,7 +10,22 @@ df = pd.read_csv(file)
 
 print(df.columns)
 
+# get contractiions
+# obtained from https://stackoverflow.com/questions/19790188/expanding-english-language-contractions-in-python/19794953#19794953
+with open('contractions.json', 'r') as f:
+    contractions_dict = json.load(f)
+
+# Define function to expand contractions
+def expand_contractions(text, contractions_dict=contractions_dict):
+    for contraction, expansion in contractions_dict.items():
+        text = text.replace(contraction, expansion)
+    return text
+
 def preprocess_text(text):
+    # expand contractions
+    text = expand_contractions(text)
+    text = contractions.fix(text)
+
     text = text.translate(str.maketrans('', '', string.punctuation))
     text = text.lower()
     doc = nlp(text)
