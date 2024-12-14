053cca96acf07f5f2a056058637edd5d7c33a384
cynthia <61641064+cor100@users.noreply.github.com>
2024-12-13 23:41:44 -0500

    application data

--- application_data.py
+++ application_data.py
@@ -1,8 +1,9 @@
 import pandas as pd
 import pandas as pd
 from transformers import AutoTokenizer, AutoModelForSequenceClassification
-import torch
 df = pd.read_csv('./csvs/processed_dataset.csv')
+scores=pd.read_csv('./csvs/global_scores.csv')
+
 tokenizer = AutoTokenizer.from_pretrained("./finetuned_global_scores_model")
 model = AutoModelForSequenceClassification.from_pretrained("./finetuned_global_scores_model")
 model.eval()  # Set the model to evaluation mode
@@ -10,13 +11,11 @@ model.eval()  # Set the model to evaluation mode
 label_counts = df.groupby(["dialog_id", "final agreed label"]).size().reset_index(name="count")
 labels = ['Give Information','Open Question','Closed Question', 'Simple Reflection', 'Closed Reflection', 'Affirm','Emphasize Autonomy','Confront']
 # Ensure all possible labels are represented in each session
-# Create a full cartesian product of dialog_id and possible labels
+#  cartesian product of dialog_id and possible labels
 unique_sessions = df["dialog_id"].unique()
 session_label_combinations = pd.MultiIndex.from_product(
     [unique_sessions, labels], names=["dialog_id", "final agreed label"]
 ).to_frame(index=False)
-
-# Merge actual counts with the full combinations
 label_counts_complete = session_label_combinations.merge(
     label_counts, on=["dialog_id", "final agreed label"], how="left"
 )
@@ -29,14 +28,15 @@ print(label_counts_complete.head())
 
 
 
-# Sort labels by count within each session
+# sort labels by count
 sorted_labels = label_counts_complete.sort_values(by=["dialog_id", "count"], ascending=[True, False])
 
-# Aggregate the sorted labels into a list for each session
+# aggregate features
 sorted_labels_per_session = sorted_labels.groupby("dialog_id")["final agreed label"].apply(list).reset_index()
-
-# Rename columns for clarity
 sorted_labels_per_session.columns = ["dialog_id", "sorted_agreed_labels"]
 
-# Verify the result
+
 print(sorted_labels_per_session.head())
+session_scores = scores.merge(sorted_labels_per_session, on='dialog_id', how='left')
+print(session_scores.head())
+session_scores.to_csv('./csvs/session_scores.csv', index=False)
